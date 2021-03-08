53b9f60d0f624a4448e6bb2eed550acf4bd78b2a
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-03-08 14:55:30 +0100

    Setup ProductListing.js and ProductListing.spec.js incl. test todos

---
+++ client/src/components/ProductListing/ProductListing.spec.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import ProductListing from './ProductListing'
+
+describe('ProductListing', () => {
+  it.todo('render a card with an image, brand, model and price')
+  it.todo('should display an overlay with available sizes on hover')
+  it.todo('render the price tag red, if on sale')
+})
