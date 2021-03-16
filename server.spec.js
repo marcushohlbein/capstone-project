0d72f16cc98a711e8c6ca6e8fb9e5adc67935dc2
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-03-16 22:56:36 +0100

    Fix server test

--- server.spec.js
+++ server.spec.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const app = require('./app')
+const app = require('./server')
 
 describe('Server', () => {
   it('should fetch all products', async () => {
@@ -12,7 +12,6 @@ describe('Server', () => {
     const postId = '60508b013d14ab54f9c33be6'
     const res = await request(app).get(`/api/v1/products/${postId}`)
     expect(res.statusCode).toEqual(200)
-    expect(res.body).toHaveProperty('styleId')
-    done()
+    expect(res.body._id).toBe('60508b013d14ab54f9c33be6')
   })
 })
