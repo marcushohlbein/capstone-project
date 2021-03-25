52b3fe43a6f2bacdc17a7bee5f8224acf45c10e1
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-03-25 12:37:30 +0100

    Small fixings

--- client/src/lib/formatedSize.js
+++ client/src/lib/formatedSize.js
@@ -1,3 +1,7 @@
 export default function formatedSize(size) {
-  return size.replace('1/3', ' ⅓').replace('2/3', ' ⅔').replace('.0', '')
+  return size
+    .replace('1/3', ' ⅓')
+    .replace('2/3', ' ⅔')
+    .replace('.0', '')
+    .replace('.', ',')
 }
